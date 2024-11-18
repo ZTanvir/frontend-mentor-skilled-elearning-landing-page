fe1e0a2ef70de2029b713c927a0ecd78b3424d3f
zahirul <zahirulopel@gmail.com>
2024-11-18 12:36:39 +0600

    add different style buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,17 @@
 import styles from "../styles/components/button.module.css";
-const Button = ({ text, color, bgColor }) => {
+const Button = ({ text, color, bgColor, hoverColor }) => {
+  const handleMouseEnter = (e) => {
+    e.currentTarget.style.background = hoverColor;
+    e.currentTarget.style.cursor = "pointer";
+  };
+
+  const handleMouseLeave = (e) => {
+    e.currentTarget.style.background = bgColor;
+  };
   return (
     <button
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
       style={{ background: bgColor, color }}
       className={styles.btnContainer}
     >
