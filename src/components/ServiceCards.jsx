4de62f844753010d99e1b402afa518f8a075b30a
zahirul <zahirulopel@gmail.com>
2024-11-20 14:37:45 +0600

    popular course card added

--- src/components/ServiceCards.jsx
+++ src/components/ServiceCards.jsx
@@ -1,8 +1,12 @@
 import ServiceCard from "./ServiceCard";
 import cardsData from "../data/cardsData";
+import styles from "../styles/components/serviceCards.module.css";
 const ServiceCards = () => {
   return (
     <>
+      <section className={styles.popularCourseCard}>
+        <p>Check out our most popular courses!</p>
+      </section>
       {cardsData.map((card) => (
         <ServiceCard
           key={card.id}
