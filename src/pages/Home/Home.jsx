0a38d82b9838979d7e409071105087d689a2077f
zahirul <zahirulopel@gmail.com>
2024-11-20 14:41:51 +0600

    add symantic html for the webpage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,15 +2,24 @@ import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import HeroSection from "./HeroSection";
 import ServiceCards from "../../components/ServiceCards";
-import photography from "../../assets/icon-photography.svg";
 
 const Home = () => {
   return (
     <>
-      <Navbar />
-      <HeroSection />
-      <ServiceCards />
-      <Footer />
+      <nav>
+        <Navbar />
+      </nav>
+      <main>
+        <section>
+          <HeroSection />
+        </section>
+        <section>
+          <ServiceCards />
+        </section>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </>
   );
 };
